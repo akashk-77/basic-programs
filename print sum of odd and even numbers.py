0fb76d8eb47055d350e30f49c565af18adae33e8
akashk-77 <akashkxy@gmail.com>
2026-05-19 20:55:21 +0530

    Add files via upload

---
+++ print sum of odd and even numbers.py
@@ -0,0 +1,16 @@
+
+n = int(input("Enter a number: "))
+
+odd_sum = 0
+even_sum = 0
+
+
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        even_sum += i
+    else:
+        odd_sum += i
+
+
+print("Sum of odd numbers:", odd_sum)
+print("Sum of even numbers:", even_sum)
